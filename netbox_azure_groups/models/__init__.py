7c57075083870ead281492daf634517dd8327f5a
Brynjar <brynjarfaune@gmail.com>
2025-08-28 13:50:49 +0200

    fix: Add correct related_name to dummy models

--- netbox_azure_groups/models/__init__.py
+++ netbox_azure_groups/models/__init__.py
@@ -7,7 +7,7 @@ from django.db import models
 # These will be replaced with the proper refactored models
 class GroupMembership(NetBoxModel):
     """Temporary dummy class - DO NOT USE"""
-    group = models.ForeignKey(AzureGroup, on_delete=models.CASCADE)
+    group = models.ForeignKey(AzureGroup, on_delete=models.CASCADE, related_name='memberships')
     member_type = models.CharField(max_length=20, choices=[('direct', 'Direct')])
     
     class Meta:
@@ -16,7 +16,7 @@ class GroupMembership(NetBoxModel):
     
 class GroupOwnership(NetBoxModel):
     """Temporary dummy class - DO NOT USE"""
-    group = models.ForeignKey(AzureGroup, on_delete=models.CASCADE)
+    group = models.ForeignKey(AzureGroup, on_delete=models.CASCADE, related_name='ownerships')
     
     class Meta:
         managed = False
