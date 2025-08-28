bd40839465be258d0251e3b0f5842f22f39181bb
Brynjar <brynjarfaune@gmail.com>
2025-08-28 12:42:46 +0200

    debug: Add logging to template extensions to identify duplicate calls

--- netbox_azure_groups/template_content.py
+++ netbox_azure_groups/template_content.py
@@ -1,6 +1,9 @@
+import logging
 from django.contrib.contenttypes.models import ContentType
 from netbox.plugins import PluginTemplateExtension
 from .models import GroupMembership, GroupOwnership
+
+logger = logging.getLogger(__name__)
 
 
 class ContactAzureGroupsExtension(PluginTemplateExtension):
@@ -9,6 +12,8 @@ class ContactAzureGroupsExtension(PluginTemplateExtension):
     def full_width_page(self):
         contact = self.context['object']
         contact_ct = ContentType.objects.get_for_model(contact)
+        
+        logger.warning(f"ContactAzureGroupsExtension.full_width_page() called for contact {contact.pk} ({contact.name})")
         
         # Get groups this contact is a member of
         memberships = GroupMembership.objects.filter(
@@ -21,6 +26,8 @@ class ContactAzureGroupsExtension(PluginTemplateExtension):
             content_type=contact_ct,
             object_id=contact.pk
         ).select_related('group')
+        
+        logger.warning(f"Found {memberships.count()} memberships and {ownerships.count()} ownerships for contact {contact.pk}")
         
         return self.render('netbox_azure_groups/inc/contact_groups.html', {
             'contact': contact,
@@ -36,11 +43,15 @@ class DeviceAzureGroupsExtension(PluginTemplateExtension):
         device = self.context['object']
         device_ct = ContentType.objects.get_for_model(device)
         
+        logger.warning(f"DeviceAzureGroupsExtension.full_width_page() called for device {device.pk} ({device.name})")
+        
         # Get groups this device is a member of
         memberships = GroupMembership.objects.filter(
             content_type=device_ct,
             object_id=device.pk
         ).select_related('group')
+        
+        logger.warning(f"Found {memberships.count()} memberships for device {device.pk}")
         
         return self.render('netbox_azure_groups/inc/device_groups.html', {
             'device': device,
