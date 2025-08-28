40f85574a8ddeeea3b498e1772e586f157d21b80
Brynjar <brynjarfaune@gmail.com>
2025-08-28 13:41:49 +0200

    temp: Add dummy model classes to prevent import errors
    - Temporary solution to get NetBox running
    - All files can still import GroupMembership and GroupOwnership
    - These dummy classes prevent functionality but allow startup
    - Full refactor needed to properly implement new model structure

--- netbox_azure_groups/models/__init__.py
+++ netbox_azure_groups/models/__init__.py
@@ -1,7 +1,17 @@
 # Temporarily simplified imports during refactoring 
 from .azure_groups import AzureGroup, GroupTypeChoices
 
-__all__ = ['AzureGroup', 'GroupTypeChoices']
+# Temporary dummy classes to prevent import errors during refactoring
+# These will be replaced with the proper refactored models
+class GroupMembership:
+    """Temporary dummy class - DO NOT USE"""
+    _meta = type('Meta', (), {'get_field': lambda self, name: type('Field', (), {'choices': []})()})()
+    
+class GroupOwnership:
+    """Temporary dummy class - DO NOT USE"""
+    _meta = type('Meta', (), {'get_field': lambda self, name: type('Field', (), {'choices': []})()})()
+
+__all__ = ['AzureGroup', 'GroupTypeChoices', 'GroupMembership', 'GroupOwnership']
 
 # TODO: Complete the refactoring of these models
 # from .azure_groups import ContactGroupMembership, ContactGroupOwnership, DeviceGroupMembership
